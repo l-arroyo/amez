8a12148e88c3efb82e42107c69c4cc0d4803c527
Lucas Arroyo <elpropiolucas@gmail.com>
2023-09-08 12:52:27 +0200

    made hero image editable from wagtail admin pannel and removed unused classes

--- amez/home/models.py
+++ amez/home/models.py
@@ -5,8 +5,14 @@ from wagtail.models import Page, Orderable
 from wagtail.images.edit_handlers import FieldPanel
 from modelcluster.fields import ParentalKey
 
-    
 class HomePage(Page):
+    hero_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
     about_us = RichTextField(blank=True)
     about_us_image = models.ForeignKey(
         'wagtailimages.Image',
@@ -23,23 +29,16 @@ class HomePage(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
-    sponsors = RichTextField(blank=True)
     
     content_panels = Page.content_panels + [
+        FieldPanel('hero_image'),
         FieldPanel('about_us'),
         FieldPanel('about_us_image'),
         FieldPanel('event_info'),
-        FieldPanel('event_image')
+        FieldPanel('event_image'),
     ]
-    
-class SponsorsGalleryImage(Orderable):
-    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='gallery_images')
-    image = models.ForeignKey(
-        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
-    )
-    caption = models.CharField(blank=True, max_length=250)
 
-    panels = [
-        FieldPanel('image'),
-        FieldPanel('caption'),
-    ]
+    def hero_image_url(self):
+        if self.hero_image:
+            return self.hero_image.file.url
+        return None
